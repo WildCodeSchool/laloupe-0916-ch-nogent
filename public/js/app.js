3ebf9b8b7099b9d4d82442692e1c477deb20bbd9
MrDrannoc <poyer.k@gmail.com>
2016-10-04 17:25:12 +0200

    Finalisation view contact

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ angular.module('app', ['ngRoute'])
         .controller('mainController', mainController)
         .controller('navbarController', navbarController)
         .controller('loginController', loginController)
+        .controller('contactController', contactController)
         .config(routes)
         .run(loginStatus)
         ;
---
+++ public/js/controllers/contactController.js
@@ -0,0 +1,11 @@
+class contactController {
+
+    constructor() {
+        $(document).ready(function() {
+            $('.collapsible').collapsible({
+                accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+            });
+        });
+    }
+
+}
