ea0d3698c4f68510618b8cb7465f4b69c6514652
CamJr <cfontainef1@gmail.com>
2016-10-04 18:05:46 +0200

    modification contact

--- public/js/controllers/contactController.js
+++ public/js/controllers/contactController.js
@@ -5,6 +5,10 @@ class contactController {
             $('.collapsible').collapsible({
                 accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
             });
+            $(document).ready(function() {
+  $('select').material_select();
+});
+        
         });
     }
 
