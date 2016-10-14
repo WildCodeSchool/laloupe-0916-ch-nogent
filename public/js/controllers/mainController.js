39537d03704f058953b772867df25fa8693b4b1c
Guillaume <fan.fan@live.fr>
2016-10-14 15:48:10 +0200

    Modal Access

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -60,7 +60,8 @@ class mainController {
 
             $(document).ready(function() {
                 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-                $('.modal-trigger').leanModal();
+                $('.modal-trigger1').leanModal();
+                $('.modal-trigger2').leanModal();
             });
 
             this.isLoad = false;
