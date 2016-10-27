57a22ede9cf446049cdd38dbca97ad67e291086e
stephsen <stephsen59@gmail.com>
2016-10-27 13:33:44 +0200

    sidebar

---
+++ public/js/controllers/admin/navbarController.js
@@ -0,0 +1,67 @@
+class navbarController {
+
+    constructor(sessionFactory, $rootScope, $window, $location, categorieService) {
+        this.categorieService = categorieService;
+        this.loadCategories();
+        this.isLogged = sessionFactory.isLogged;
+        this.sessionFactory = sessionFactory;
+        this.$rootScope = $rootScope;
+        this.$location = $location;
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > $('#scrollspy').height()) {
+                $('#push').addClass('navbar-fixed');
+                if ($('#urgenceHide').hasClass('ng-hide')) {
+                  $('#urgenceHide').addClass('ng-hide');
+                  $('.group-btn').show();
+                }
+                $('#topFixed').css({
+                    'margin-top': '100px'
+                });
+            }
+            if ($(window).scrollTop() < $('#scrollspy').height()) {
+                $('#push').removeClass('navbar-fixed');
+                // $('#urgenceHide').removeClass('ng-hide');
+                $('#topFixed').css({
+                    'margin-top': '0'
+                });
+            }
+        });
+
+
+        $rootScope.$on('loginStatusChanged', (event, isLogged) => {
+            this.isLogged = isLogged;
+            this.user = sessionFactory.user;
+        });
+
+        this.isToggled = false;
+        this.toggleEmergencies = () => {
+            this.isToggled = !this.isToggled;
+            console.log(this.isToggled);
+            if (this.isToggled == true) {
+              $('.group-btn').hide();
+            } else {
+              $('.group-btn').show();
+            }
+        };
+    }
+
+    logout() {
+        this.sessionFactory.isLogged = false;
+        this.sessionFactory.user = {};
+        this.sessionFactory.token = null;
+        this.$rootScope.$emit('loginStatusChanged', false);
+        this.isLogged = false;
+        this.$location.path('/login');
+    }
+
+    loadCategories() {
+        this.categorieService.getChildrenOf('0').then((res) => {
+            this.categories = res.data;
+            setTimeout(function() {
+                $(".dropdown-button").dropdown();
+            }, 0);
+        });
+    }
+
+}
---
+++ public/js/controllers/admin/sidebarController.js
@@ -0,0 +1,29 @@
+class sidebarController {
+
+    constructor(sessionFactory, $rootScope, $window, $location, categorieService) {
+        this.categorieService = categorieService;
+        this.isLogged = sessionFactory.isLogged;
+        this.sessionFactory = sessionFactory;
+        this.$rootScope = $rootScope;
+        this.$location = $location;
+
+        $(".button-collapse").sideNav();
+
+        $rootScope.$on('loginStatusChanged', (event, isLogged) => {
+            this.isLogged = isLogged;
+            this.user = sessionFactory.user;
+        });
+
+    }
+
+    logout() {
+        this.sessionFactory.isLogged = false;
+        this.sessionFactory.user = {};
+        this.sessionFactory.token = null;
+        this.$rootScope.$emit('loginStatusChanged', false);
+        this.isLogged = false;
+        this.$location.path('/login');
+    }
+
+
+}
