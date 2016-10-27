a501c15ca53542ccc991278f00fc850934619652
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-10-27 21:28:43 +0200

    Merge HB

--- public/js/routes.admin.js
+++ public/js/routes.admin.js
@@ -94,7 +94,7 @@ const routes = ($routeProvider, $httpProvider) => {
         })
         .when('/marches', {
             templateUrl: '../views/admin/marches.html',
-            controller: 'marchesController',
+            controller: 'marcheController',
             controllerAs: 'vm',
             resolve: {
                 connected: checkIsConnected
