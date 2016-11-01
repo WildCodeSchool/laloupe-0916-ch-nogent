a0057fdf5eb3f7e118a337809364c5897d96fe1f
MrDrannoc <poyer.k@gmail.com>
2016-11-01 16:23:09 +0100

    ajout vue adm user

--- public/js/routes.admin.js
+++ public/js/routes.admin.js
@@ -22,7 +22,7 @@ const routes = ($routeProvider, $httpProvider) => {
         })
         .when('/utilisateurs', {
             templateUrl: '../views/admin/utilisateurs/show.html',
-            controller: '',
+            controller: 'listuserController',
             controllerAs: 'vm',
             resolve: {
                 connected: checkIsConnected
