d7d83257725b353136afecd254f0e37e5747c21e
stephsen <stephsen59@gmail.com>
2016-11-02 11:17:52 +0100

    upload img a propos et actualites

--- public/js/controllers/admin/actualiteController.js
+++ public/js/controllers/admin/actualiteController.js
@@ -18,7 +18,36 @@ class actualiteController {
 
         });
 
+        function uploadFile(file) {
+            var url = '/api/picture';
+            var xhr = new XMLHttpRequest();
+            var fd = new FormData();
+            xhr.open("POST", url, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    // Every thing ok, file uploaded
+                    console.log(xhr.responseText); // handle response.
+                }
+            };
+            fd.append("upload_file", file);
+            xhr.send(fd);
+        }
+
+        var uploadfiles = document.querySelector('#uploadImage');
+        uploadfiles.addEventListener('change', function() {
+            var files = this.files;
+            for (var i = 0; i < files.length; i++) {
+                uploadFile(this.files[i]); // call the function to upload the file
+            }
+        }, false);
+
     }
+
+    uplodFileChangeImage() {
+      this.UploadImg = '/uploads/img_' + document.getElementById('uploadImage').value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
+    }
+
+
     load() {
         this.actualiteService.getAll().then((res) => {
             this.actualites = res.data;
--- public/js/controllers/admin/aproposController.js
+++ public/js/controllers/admin/aproposController.js
@@ -8,7 +8,35 @@ class aproposController {
             plugins: 'advlist autolink link image lists charmap autoresize textcolor'
         };
         this.load();
+
+        function uploadFile(file) {
+            var url = '/api/picture';
+            var xhr = new XMLHttpRequest();
+            var fd = new FormData();
+            xhr.open("POST", url, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    // Every thing ok, file uploaded
+                    console.log(xhr.responseText); // handle response.
+                }
+            };
+            fd.append("upload_file", file);
+            xhr.send(fd);
+        }
+
+        var uploadfiles = document.querySelector('#uploadImage');
+        uploadfiles.addEventListener('change', function() {
+            var files = this.files;
+            for (var i = 0; i < files.length; i++) {
+                uploadFile(this.files[i]); // call the function to upload the file
+            }
+        }, false);
     }
+
+    uplodFileChangeImage() {
+      this.UploadImg = '/uploads/img_' + document.getElementById('uploadImage').value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
+    }
+    
     load() {
         this.aproposService.getAll().then((res) => {
             this.aproposs = res.data;
