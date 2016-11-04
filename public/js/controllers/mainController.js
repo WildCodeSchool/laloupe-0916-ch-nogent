12cbaa36c7cdf7d42580b7bc424bf6ebb9c0adce
stephsen <stephsen59@gmail.com>
2016-11-04 16:12:16 +0100

    fix fermeture menu mobile

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -8,7 +8,13 @@ class mainController {
             this.statService = statService;
         this.load();
 
-        $(".button-collapse").sideNav();
+        $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+          }
+        );
 
         $('.slider').slider({
             full_width: true,
